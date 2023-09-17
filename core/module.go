95146972c88601d1c0199d8f1312eb03cf6392ae
Jens Heise <k@jensheise.com>
2023-09-17 23:56:11 +0200

    Add comments to module.go

--- core/module.go
+++ core/module.go
@@ -10,35 +10,52 @@ import (
 
 const MODULE_MOD = "sys.core.module"
 
+// Module is an interface representing components that can be managed by the ModuleManager.
+// Implementing the Module interface allows for lifecycle management including setup, start and stop operations.
 type Module interface {
+	// ID returns a unique identifier for the module.
 	ID() string
+	// Setup prepares the module for operation. It is called before Start.
 	Setup(args *ModLifecycleArgs, ctx context.Context) error
+	// Start activates the module. It is called after Setup and before Stop.
 	Start(args *ModLifecycleArgs, ctx context.Context) error
+	// Stop deactivates the module. It is called after Start.
+	// Stop should be used for cleanup tasks.
 	Stop(args *ModLifecycleArgs) error
 }
 
+// ModuleManager is responsible for managing the lifecycle of registered modules.
+// It ensures modules are set up, started and stopped in a controlled manner.
 type ModuleManager struct {
-	mu      sync.Mutex
+	mu sync.Mutex
+	// Collection of registered modules indexed by their ID
 	modules map[string]Module
-	setup   bool
+	// Flag indicating if modules have been set up
+	setup bool
 }
 
+// ModLifecycleArgs holds arguments that are passed to module lifecycle methods.
 type ModLifecycleArgs struct {
 	Logger trace.Logger
 }
 
 var modules = NewManager()
 
+// Manager provides a singleton instance of ModuleManager.
 func Manager() *ModuleManager {
 	return modules
 }
 
+// NewManager creates and returns a new instance of ModuleManager.
 func NewManager() *ModuleManager {
 	return &ModuleManager{
 		modules: make(map[string]Module),
 	}
 }
 
+// Register registers one or more modules with the manager.
+// It's essential to register modules before setting them up.
+// Registering after setup will result in a panic.
 func (m *ModuleManager) Register(modules ...Module) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -55,6 +72,9 @@ func (m *ModuleManager) Register(modules ...Module) {
 	}
 }
 
+// Setup initializes all registered modules.
+// This method should be called before starting the modules.
+// Calling Setup after the modules have been setup will result in a panic.
 func (m *ModuleManager) Setup(args *ModLifecycleArgs, ctx context.Context) []error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -75,6 +95,8 @@ func (m *ModuleManager) Setup(args *ModLifecycleArgs, ctx context.Context) []err
 	return errs
 }
 
+// Start activates all registered modules.
+// It's essential to set up the modules before starting otherwise a panic will occur.
 func (m *ModuleManager) Start(args *ModLifecycleArgs, ctx context.Context) []error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -93,6 +115,8 @@ func (m *ModuleManager) Start(args *ModLifecycleArgs, ctx context.Context) []err
 	return errs
 }
 
+// Stop deactivates all registered modules.
+// Modules should be started before they can be stopped otherwise a panic will occur.
 func (m *ModuleManager) Stop(args *ModLifecycleArgs) []error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
