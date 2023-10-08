a01d0674bc68944153e83976b29841e5b188d60c
Jens Heise <k@jensheise.com>
2023-10-08 15:47:47 +0200

    Wrap utility function

--- core/util/util.go
+++ core/util/util.go
@@ -2,6 +2,7 @@ package util
 
 import "fmt"
 
+// Unwrap panics if the error is not nil. Otherwise, it returns the value.
 func Unwrap[T any](v T, e error) T {
 	if e != nil {
 		panic(fmt.Errorf("panic on error where error is not expected, check misconfigurations, see error: %w", e))
@@ -10,10 +11,20 @@ func Unwrap[T any](v T, e error) T {
 	return v
 }
 
+// Ok panics if the error is not nil.
 func Ok(e error) {
 	if e == nil {
 		return
 	}
 
 	panic(fmt.Errorf("panic on error where error is not expected, check misconfigurations, see error: %w", e))
+}
+
+// Wrap wraps an error with a message.
+func Wrap(e error, msg string) error {
+	if e == nil {
+		return nil
+	}
+
+	return fmt.Errorf("%s: %w", msg, e)
 }
