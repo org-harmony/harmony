5e2df6c015b293dc8b79b67017fb6f85184165d6
Jens Heise <k@jensheise.com>
2023-10-17 00:18:03 +0200

    dynamically show providers for login from config add display name for providers fix error in error.go.html template

--- core/auth/auth.go
+++ core/auth/auth.go
@@ -21,14 +21,15 @@ const (
 
 // Cfg is the config for the auth package.
 type Cfg struct {
-	// Provider contains a list of OAuth2 providers.
-	Provider     map[string]ProviderCfg `toml:"provider"`
+	// Providers contains a list of OAuth2 providers.
+	Providers    map[string]ProviderCfg `toml:"provider"`
 	EnableOAuth2 bool                   `toml:"enable_oauth2"`
 }
 
 // ProviderCfg is the config for an OAuth2 provider.
 type ProviderCfg struct {
 	Name           string   `toml:"name" validate:"required"`
+	DisplayName    string   `toml:"display_name" validate:"required"`
 	AuthorizeURI   string   `toml:"authorize_uri" validate:"required"`
 	AccessTokenURI string   `toml:"access_token_uri" validate:"required"`
 	UserinfoURI    string   `toml:"userinfo_uri" validate:"required"`
@@ -46,23 +47,23 @@ func RegisterAuth(appCtx ctx.App, webCtx web.Context) {
 
 func registerRoutes(cfg *Cfg, appCtx ctx.App, webCtx web.Context) {
 	router := webCtx.Router()
-	router.Get("/auth/login", loginController(appCtx, webCtx).ServeHTTP)
+	router.Get("/auth/login", loginController(cfg, appCtx, webCtx).ServeHTTP)
 
 	if !cfg.EnableOAuth2 {
 		return
 	}
 
-	providers := cfg.Provider
+	providers := cfg.Providers
 	router.Get(fmt.Sprintf(OAuthLoginPattern, "{provider}"), oAuthLoginController(appCtx, webCtx, providers).ServeHTTP)
 	router.Get(fmt.Sprintf(OAuthLoginSuccessPattern, "{provider}"), oAuthLoginSuccessController(appCtx, webCtx, providers).ServeHTTP)
 }
 
-func loginController(appCtx ctx.App, webCtx web.Context) http.Handler {
+func loginController(cfg *Cfg, appCtx ctx.App, webCtx web.Context) http.Handler {
 	loginT := util.Unwrap(util.Unwrap(webCtx.TemplaterStore().Templater(web.LandingPageTemplateName)).
 		Template("auth.login", "auth/login.go.html"))
 
 	return web.NewController(appCtx, webCtx, func(io web.IO) error {
-		return io.Render(loginT, nil)
+		return io.Render(loginT, cfg)
 	})
 }
 
@@ -73,7 +74,8 @@ func oAuthLoginController(appCtx ctx.App, webCtx web.Context, providers map[stri
 	return web.NewController(appCtx, webCtx, func(io web.IO) error {
 		oAuthCfg, _, err := oAuthConfigByProviderName(
 			webCtx.Router().URLParam(io.Request(), "provider"),
-			providers, webCtx.Configuration().Server.BaseURL,
+			providers,
+			webCtx.Configuration().Server.BaseURL,
 		)
 		if err != nil {
 			return io.Error(errT, web.ExtErr("auth.error.invalid-provider"))
@@ -96,7 +98,8 @@ func oAuthLoginSuccessController(appCtx ctx.App, webCtx web.Context, providers m
 
 		oAuthCfg, provider, err := oAuthConfigByProviderName(
 			webCtx.Router().URLParam(io.Request(), "provider"),
-			providers, webCtx.Configuration().Server.BaseURL,
+			providers,
+			webCtx.Configuration().Server.BaseURL,
 		)
 		if err != nil {
 			return io.Error(errT, web.ExtErr("auth.error.invalid-provider"))
